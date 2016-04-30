23a97812ea3046394b2cf52677d54add14a0be0b
joshiejack <joshjackwildman@gmail.com>
2016-04-30 17:21:02 +0100

    Add support for loading potion effects created in 1.9.0

--- src/main/java/joshie/progression/criteria/filters/potion/FilterPotionEffectID.java
+++ src/main/java/joshie/progression/criteria/filters/potion/FilterPotionEffectID.java
@@ -1,13 +1,16 @@
 package joshie.progression.criteria.filters.potion;
 
+import com.google.gson.JsonObject;
 import joshie.progression.api.criteria.ProgressionRule;
+import joshie.progression.api.special.ISpecialJSON;
+import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 
 import java.util.Arrays;
 import java.util.Collection;
 
 @ProgressionRule(name="potioneffect", color=0xFFFF73FF)
-public class FilterPotionEffectID extends FilterPotionBase {
+public class FilterPotionEffectID extends FilterPotionBase implements ISpecialJSON {
     public int effectID = 1;
 
     @Override
@@ -19,4 +22,22 @@ public class FilterPotionEffectID extends FilterPotionBase {
     public Collection<PotionEffect> getRandomEffects() {
         return Arrays.asList(new PotionEffect(effectID, 200, 0));
     }
+
+    @Override
+    public boolean onlySpecial() {
+        return false;
+    }
+
+    @Override
+    public void readFromJSON(JsonObject object) {
+        if (object.get("name") != null) {
+            try {
+                String name = object.get("name").getAsString();
+                effectID = Potion.getPotionFromResourceLocation(name).id;
+            } catch (Exception e) {}
+        }
+    }
+
+    @Override
+    public void writeToJSON(JsonObject object) {}
 }
